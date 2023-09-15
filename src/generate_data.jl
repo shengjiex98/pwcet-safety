cc1fb8f037592f26a17e4b05cb147ef9883a5146
Shengjie Xu <shengjiex98@gmail.com>
2023-09-15 16:36:15 -0400

    Save small batch data

--- src/generate_data.jl
+++ src/generate_data.jl
@@ -18,14 +18,22 @@ sp = SamplerPWCET(0.9, 100)
 
 @info Threads.nthreads()
 
-n = 1_000_000
-@time res99 = generate_samples(a, z0, 0.99, n)
-@time res90 = generate_samples(a, z0, 0.90, n)
-@time res50 = generate_samples(a, z0, 0.50, n)
-@time res10 = generate_samples(a, z0, 0.10, n)
-
 using Serialization
-@time serialize("../data/res99-$n.jls", res99)
-@time serialize("../data/res90-$n.jls", res90)
-@time serialize("../data/res50-$n.jls", res50)
-@time serialize("../data/res10-$n.jls", res10)
+
+# n = 1_000_000
+# @time res99 = generate_samples(a, z0, 0.99, n)
+# @time res90 = generate_samples(a, z0, 0.90, n)
+# @time res50 = generate_samples(a, z0, 0.50, n)
+# @time res10 = generate_samples(a, z0, 0.10, n)
+
+# @time serialize("../data/res99-$n.jls", res99)
+# @time serialize("../data/res90-$n.jls", res90)
+# @time serialize("../data/res50-$n.jls", res50)
+# @time serialize("../data/res10-$n.jls", res10)
+
+batch_size = 10000
+@time batches99 = map(_ -> generate_samples(a, z0, 0.99, batch_size), 1:100)
+@time batches90 = map(_ -> generate_samples(a, z0, 0.90, batch_size), 1:100)
+
+serialize("../data/batches/batches99.jls", batches99)
+serialize("../data/batches/batches90.jls", batches90)
