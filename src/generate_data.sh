dd331efddeead998ed37d6796d02aa93a45e0156
Shengjie Xu <shengjiex98@gmail.com>
2024-03-03 05:11:16 -0500

    More data

--- src/generate_data.sh
+++ src/generate_data.sh
@@ -13,8 +13,10 @@ do
         for n in 100
         do
             for q in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 0.99 0.999
+            # for q in 0.15 0.25 0.35 0.45 0.55 0.65 0.75 0.85 0.95
             do
                 for h in 0.005 0.01 0.015 0.02 0.025 0.03 0.035 0.04 0.045 0.05 0.1 0.2 0.5 1.0
+                # for h in 0.0075 0.0125 0.0175 0.0225 0.0275 0.0325 0.0375 0.0425 0.0475 0.0525 0.055 0.0575 0.06
                 do
                     # 20GB of memory, 16 core, and a one day time limit.
                     (set -x; sbatch -p general -c $th -t 01-00:00:00 --mem=8g --wrap="julia --project --threads=$th generate_data.jl normal $b $q $h")
