d5c3480e79bd5f064d765e4c098eb4dd1b71d94f
Shengjie Xu <shengjiex98@gmail.com>
2024-04-15 00:01:35 -0400

    Clean up notebook. make ready for multi-system presentation

--- notebooks/visualization.jl
+++ notebooks/visualization.jl
@@ -19,115 +19,140 @@ begin
 	import Pkg
 	Pkg.activate("..")
 
-	using PlutoUI
 	using Printf
 	using Serialization
 	using DelimitedFiles
 	using Distributions: Distribution, Normal, Pareto, Uniform, cdf, pdf, quantile
-	# using PlotlyJS
 	using Plots
 	plotlyjs()
 
 	push!(LOAD_PATH, "../src")
 	using Experiments
 	
+	using PlutoUI
+	TableOfContents()
 end
 
-# ╔═╡ 55e5c7d9-969c-415e-8822-de80174e8710
-begin
-	path = "../data/nmc-samenom"
+# ╔═╡ 0becd1ea-ade9-43d4-9039-fb335de62c6d
+md"""
+# Visualization for Experiment Results
+"""
 
-	b = 100_000
+# ╔═╡ 305d3f52-0208-4fe7-b362-1f9b9424b705
+md"""
+## Load Packages
+"""
+
+# ╔═╡ 9bd08151-ce7f-4d57-9dd7-7af79ff42d3f
+md"""
+## Defining Plotting Function
+"""
+
+# ╔═╡ ce5e566b-b1e3-4e79-9156-4237a170546e
+function plot_results(
+		q_values::Vector{<:Real},
+		h_values::Vector{<:Real},
+		quantiles::Vector{<:Real};
+		filter_fn::Union{Function, Nothing}=nothing,
+		confidence::Union{Matrix{<:Real}, Nothing}=nothing,
+		color::Union{Symbol, Vector{Symbol}}=:lightblue,
+		title::String="",
+		draw_surface::Bool=true,
+		draw_all_points::Bool=true,
+		mode::String="free",
+		az::Real=55,
+		el::Real=15)
+
+	# Filter data if needed; `selection` is a BitVector representing points to show
+	if !isnothing(filter_fn)
+		selection = filter_fn.(q_values, h_values, quantiles)
+		q_values = q_values[selection]
+		h_values = h_values[selection]
+		quantiles = quantiles[selection]
+		if color isa Vector
+			color = color[selection]
+		end
+		if confidence isa Matrix
+			confidence = confidence[selection,:]
+		end
+	end
+	
+	# Set common properties
+	plt = plot(title=title, legend=:topleft, proj_type=:ortho)
+
+	hovertext=map(q_values, h_values, quantiles) do q, h, dev
+		@sprintf "q=%.3f h=%.3f dev=%.3f" q h dev
+	end
+	
+	# Show different graph depending on `mode`
+	if mode == "q"
+		scatter!(q_values, quantiles,
+			# xlim=(qmin, qmax), 
+			# ylim=(0, maximum(quantiles)),
+			xlabel="quantile", ylabel="deviation", color=color,
+			hover=hovertext
+		)
+		if !isnothing(confidence)
+			plot!(q_values, quantiles,
+				ribbon=(confidence[:,1], confidence[:,2]))
+		end
+	elseif mode == "period"
+		scatter!(h_values, quantiles,
+			# xlim=(hmin, hmax), 
+			# ylim=(0, maximum(quantiles)),
+			xlabel="period", ylabel="deviation", color=color)
+		if !isnothing(confidence)
+			plot!(h_values, quantiles,
+				ribbon=(confidence[:,1], confidence[:,2]))
+		end
+	elseif mode == "free"
+	    if draw_surface
+	        surface!(q_values, h_values, quantiles)
+	    end
+		scatter!(q_values, h_values, quantiles,
+			markersize=2.5,
+			# xlim=(qmin, qmax), ylim=(hmin, hmax), 
+			# zlim=(0, maximum(quantiles)),
+			xlabel="quantile", ylabel="period", zlabel="deviation",
+			color=color)
+			plot!(camera=(az, el))
+	else
+		throw(ArgumentError("`mode` has to be `free`, `q`, or `period`. $mode is given"))
+	end
+	plt
+end
+
+# ╔═╡ 4cdad6fd-9976-4bbf-b6de-95cb6a973fc7
+md"""
+## Plot Data
+
+We select a distribution and plot its PDF/CDF below:
+"""
+
+# ╔═╡ aca1dd0e-89b9-4a66-ae0b-2af07cea3636
+# dist = Normal(0, 0)
+# dist = Normal(0.03, 0.005)
+dist = Pareto(1.5, 0.03)
+# dist = Uniform(0, 0.06)
+
+# ╔═╡ 9c608fab-8fc9-4bf9-be68-96468257e6a8
+let hs = range(quantile.(dist, [0.01, 0.99])..., 100)
+	fig1 = plot(hs, pdf.(dist, hs), xlabel="T", ylabel="pdf", label="")
+	fig2 = plot(hs, cdf.(dist, hs), xlabel="T", ylabel="cdf", label="")
+	plot(fig1, fig2, plot_title="PDF/CDF for $dist")
+end
+
+# ╔═╡ 7b5d607e-da02-4060-8a9c-60c541fe34aa
+begin
+	b = 1_000_000
 	p = 0.99
-
-	qs = append!(collect(0.1:0.05:0.9), [0.99, 0.999])
-	hs = collect(0.005:0.0025:0.06)
-	# hs = append!(collect(0.005:0.0025:0.06), [0.1, 0.2, 0.5, 1.0]
-	@info "Number of quantiles and periods:" size(qs,1) size(hs,1)
-	
-	get_quantile(b, q, h; cap=Inf) = let
-	    filename = generate_filename(b, q, h, th=16)
-		if !isfile("$path/$filename.jls")
-			@info "Parameters without valid data" b q h
-			return Inf
-		end
-	    min(deserialize("$path/$filename.jls")[round(Int64, b * p)][2], cap)
-	end
-
-	readback = true
-	if !readback
-		let
-			points = Base.product(qs, hs) |> collect |> vec |> stack
-			quantiles = get_quantile.(b, points[1,:], points[2,:])
-			serialize("points.jls", (points, quantiles))
-		end
-	end
-	
-	points_small, quantiles_small = deserialize("../data-proxy/points.jls")
-end
-
-# ╔═╡ d8e29adb-6cf3-4e0c-a077-72c6606c220e
-begin
-	b_large = 1_000_000
-
-	readback_large = true
-	if !readback_large
-		let
-			qs = collect(0.7:0.01:0.8)
-			hs = append!(collect(0.02:0.001:0.03), [0.0225, 0.0275])
-			@info "Number of quantiles and periods:" size(qs,1) size(hs,1)
-			
-			points = Base.product(qs, hs) |> collect |> vec |> stack
-			quantiles = get_quantile.(b_large, points[1,:], points[2,:])
-			serialize("points_large.jls", (points, quantiles))
-		end
-	end
-	points_large, quantiles_large = deserialize("../data-proxy/points_large.jls")
-end
-
-# ╔═╡ 3f0c133d-6482-4c1d-95cf-d7802799e2f8
-begin
-	points = [points_small points_large]
-	quantiles = vcat(quantiles_small, quantiles_large)
-end
-
-# ╔═╡ 730899e1-edd5-4ddc-be98-b193a22bb6f8
-begin
-	i_99 = round(Int64, b_large*p)
-	i_low, i_high = find_intervals(b_large, p, 0.05, centered=true)[1]
-end
-
-# ╔═╡ 297e0882-2eb5-4f54-9b9f-91840659b34a
-let
-	q = 0.7
-	h = 0.0225
-	filename = generate_filename(b_large, q, h, th=16)
-	data = deserialize("$path/$filename.jls")
-	[data[i][2] for i in (i_low, i_99, i_high)]
-end
-
-# ╔═╡ 0c83a619-0405-4039-90fd-a09676eb9326
-let
-	q = 0.75
-	h = 0.0275
-	filename = generate_filename(b_large, q, h, th=16)
-	data = deserialize("$path/$filename.jls")
-	[data[i][2] for i in (i_low, i_99, i_high)]
-end
-
-# ╔═╡ f8cbd064-2eff-4ce3-8a2b-a942d8ccb47d
-let
-	q = 0.79
-	h = 0.028
-	filename = generate_filename(b_large, q, h, th=16)
-	data = deserialize("$path/$filename.jls")
-	[data[i][2] for i in (i_low, i_99, i_high)]
+	qs = 0.01:0.01:0.99
+	hs = quantile.(dist, qs)
 end
 
 # ╔═╡ d4549398-3ca3-44ba-b016-ca55bf4056cf
 md"""
 $(@bind go Button("Reset"))
-$(@bind sv Button("Save"))
 """
 
 # ╔═╡ dde0af91-d0ff-4e4d-a4d4-f8fb770987dd
@@ -148,95 +173,12 @@ let go
 	"""
 end
 
-# ╔═╡ ce5e566b-b1e3-4e79-9156-4237a170546e
-function plot_results(
-		q_values::Vector{<:Real},
-		h_values::Vector{<:Real},
-		quantiles::Vector{<:Real};
-		filter_fn::Union{Function, Nothing}=nothing,
-		confidence::Union{Matrix{<:Real}, Nothing}=nothing,
-		color::Union{Symbol, Vector{Symbol}}=:lightblue,
-		title::String="",
-		draw_surface::Bool=true,
-		draw_all_points::Bool=true,
-		mode::String="free",
-		az::Union{Real, AbstractRange{<:Real}}=55,
-		el::Real=15)
-
-	# Filter data if needed; `selection` is a BitVector representing points to show
-	if !isnothing(filter_fn)
-		selection = filter_fn.(q_values, h_values, quantiles)
-		q_values = q_values[selection]
-		h_values = h_values[selection]
-		quantiles = quantiles[selection]
-		if color isa Vector
-			color = color[selection]
-		end
-		if confidence isa Matrix
-			confidence = confidence[selection,:]
-		end
-	end
-	# Set common properties
-	plt = plot(title=title, legend=:topleft, proj_type=:ortho)
-
-	hovertext=map(q_values, h_values, quantiles) do q, h, dev
-		@sprintf "q=%.3f h=%.3f dev=%.3f" q h dev
-	end
-	
-	# Show different graph depending on `mode`
-	if mode == "q"
-		scatter!(q_values, quantiles,
-			xlim=(qmin, qmax), 
-			# ylim=(0, maximum(quantiles)),
-			xlabel="quantile", ylabel="deviation", color=color,
-			hover=hovertext
-		)
-		if !isnothing(confidence)
-			plot!(q_values, quantiles,
-				ribbon=(confidence[:,1], confidence[:,2]))
-		end
-	elseif mode == "period"
-		scatter!(h_values, quantiles,
-			xlim=(hmin, hmax), 
-			# ylim=(0, maximum(quantiles)),
-			xlabel="period", ylabel="deviation", color=color)
-		if !isnothing(confidence)
-			plot!(h_values, quantiles,
-				ribbon=(confidence[:,1], confidence[:,2]))
-		end
-	elseif mode == "free"
-	    if draw_surface
-	        surface!(q_values, h_values, quantiles)
-	    end
-		scatter!(q_values, h_values, quantiles,
-			markersize=2.5,
-			xlim=(qmin, qmax), ylim=(hmin, hmax), 
-			# zlim=(0, maximum(quantiles)),
-			xlabel="quantile", ylabel="period", zlabel="deviation",
-			color=color)
-		if az isa Real
-			plot!(camera=(az, el))
-		else
-			@gif for i in -50:10:150
-				plot!(camera=(i,el))
-			end fps=4
-		end
-
-	else
-		throw(ArgumentError("`mode` has to be `free`, `q`, or `period`. $mode is given"))
-	end
-end
-
-# ╔═╡ 87de4ba4-7f8e-4d8f-9bda-900df72339f9
-let
-	q = 0.70
-	prob_con_miss(n, q) = (1-q)^n*q
-	sum(n -> prob_con_miss(n, q), 0:3) - 0.99
-end
-
 # ╔═╡ d12e5d80-7099-4564-8144-778c812b106c
 let
-	full_matrix = readdlm("../data-proxy/nmc-dist.csv", ',')
+	SYS = "CC2"
+	# Hack to remove the prefix "Distributions." from distribution name
+	DISTNAME = join(split("$dist", ".")[2:end], ".")
+	full_matrix = readdlm("../data-proxy/nmc-dist-$SYS-$DISTNAME.csv", ',')
 	colors = let
 		res = fill(:lightblue, size(full_matrix, 1))
 		mindev = cap
@@ -254,58 +196,24 @@ let
 		dev <= cap
 	plot_results(full_matrix[:,1], full_matrix[:,2], full_matrix[:,4],
 		confidence=[full_matrix[:,4]-full_matrix[:,3] full_matrix[:,5]-full_matrix[:,4]],
-		filter_fn=filter_fn, title="Pareto(1.5, 0.01)", draw_surface=surf,
+		filter_fn=filter_fn, title="CC2", draw_surface=surf,
 		mode=mode, az=az, el=el, color=colors)
-end
-
-# ╔═╡ d0d10130-ef6e-4a7f-b3f9-4715f01b737c
-if sv == "Save"
-	savefig("illustration.pdf")
-end
-
-# ╔═╡ aca1dd0e-89b9-4a66-ae0b-2af07cea3636
-# dist = Normal(0, 0)
-# dist = Normal(0.03, 0.005)
-dist = Pareto(1.5, 0.01)
-# dist = Uniform(0, 0.06)
-
-# ╔═╡ 52349a66-88a5-4d88-8523-572e3cb6572b
-let
-	path="../data/nmc-dist/Pareto{Float64}(α=1.5, θ=0.01)"
-	q = 0.70
-	h = quantile(dist, q)
-	filename = generate_filename(b_large, q, h, th=16)
-	data = deserialize("$path/$filename.jls")
-	[data[i] for i in (i_low, i_99, i_high)]
-	function count_con_zeros(bv)
-		# Note: learn about why `let` makes this assignment
-		# using outside scope by default unless prefixed with `local`
-		local cnt = 0
-		for b in bv
-			if b
-				return cnt
-			end
-			cnt += 1
-		end
-		return cnt
-	end
-	cnt = 0
-	for i in i_99:b_large
-		if count_con_zeros(data[i][1]) < 4
-			cnt += 1
-			# @info "Found" i data[i]
-		end
-	end
-	println(cnt/b_large)
 end
 
 # ╔═╡ ae494460-6f76-4562-a6b4-42f0bc6df262
 let
+	dist = Pareto(1.5, 0.01)
+	
+	points_small, quantiles_small = deserialize("../data-proxy/dev-quantiles.jls")
+	points_large, quantiles_large = deserialize("../data-proxy/dev-quantiles-zoom.jls")
+	points = [points_small points_large]
+	quantiles = vcat(quantiles_small, quantiles_large)
+	
 	pts, qts = points, quantiles
 	filter_fn = (q, h, dev) -> 
 		q < cdf(dist, h) &&
-		qmin <= q <= qmax && 
-		hmin <= h <= hmax &&
+		# qmin <= q <= qmax && 
+		# hmin <= h <= hmax &&
 		dev <= cap
 	colors = let
 		res = fill(:lightblue, length(qts))
@@ -324,34 +232,87 @@ let
 		title="$dist", draw_surface=surf, mode=mode, az=az, el=el)
 end
 
-# ╔═╡ 9c608fab-8fc9-4bf9-be68-96468257e6a8
-let x = range(0, 0.06, 100)
-	
-	plot(x, pdf.(dist, x), title="CDF for $dist", xlabel="T", ylabel="Q")
+# ╔═╡ 2e3b2b76-c100-4f6d-b5a6-8263da6ca724
+md"""
+## Explore Specific Data Points
+
+This section requires full simulation results (in `../data/`), not just proxy
+values from `../data-proxy/`.
+"""
+
+# ╔═╡ 730899e1-edd5-4ddc-be98-b193a22bb6f8
+begin
+	i_99 = round(Int64, b * p)
+	i_low, i_high = find_intervals(b, p, 0.05, centered=true)[1]
 end
 
-# ╔═╡ ba0db11e-4098-4ead-854f-022c123e4d62
-let q = 0:0.01:0.99
-	plot(quantile.(dist, q), q, xlabel="T", ylabel="Q")
+# ╔═╡ 87de4ba4-7f8e-4d8f-9bda-900df72339f9
+let
+	q = 0.70
+	prob_con_miss(n, q) = (1-q)^n*q
+	sum(n -> prob_con_miss(n, q), 0:3) - 0.99
+end
+
+# ╔═╡ 0c83a619-0405-4039-90fd-a09676eb9326
+# ╠═╡ skip_as_script = true
+#=╠═╡
+let
+	path = "../data/nmc-dist/"
+	# q, h = 0.7, 0.0225
+	q, h = 0.75, 0.0275
+	# q, h = 0.79, 0.028
+	filename = generate_filename(b, q, h, th=16)
+	data = deserialize("$path/$filename.jls")
+	[data[i][2] for i in (i_low, i_99, i_high)]
+end
+  ╠═╡ =#
+
+# ╔═╡ b34323cf-913f-4a1b-b385-f3ae52f320ac
+let
+	path="../data/nmc-dist/Pareto{Float64}(α=1.5, θ=0.01)"
+	q = 0.70
+	h = quantile(dist, q)
+	filename = generate_filename(b, q, h, th=16)
+	data = deserialize("$path/$filename.jls")
+	[data[i] for i in (i_low, i_99, i_high)]
+	function count_con_zeros(bv)
+		# Note: learn about why `let` makes this assignment
+		# using outside scope by default unless prefixed with `local`
+		local cnt = 0
+		for b in bv
+			if b
+				return cnt
+			end
+			cnt += 1
+		end
+		return cnt
+	end
+	cnt = 0
+	for i in i_99:b
+		if count_con_zeros(data[i][1]) < 4
+			cnt += 1
+			# @info "Found" i data[i]
+		end
+	end
+	println(cnt/b)
 end
 
 # ╔═╡ Cell order:
+# ╟─0becd1ea-ade9-43d4-9039-fb335de62c6d
+# ╟─305d3f52-0208-4fe7-b362-1f9b9424b705
 # ╠═b216a1aa-dc98-11ee-0312-21d71fee5020
-# ╠═55e5c7d9-969c-415e-8822-de80174e8710
-# ╠═d8e29adb-6cf3-4e0c-a077-72c6606c220e
-# ╠═3f0c133d-6482-4c1d-95cf-d7802799e2f8
-# ╠═730899e1-edd5-4ddc-be98-b193a22bb6f8
-# ╠═297e0882-2eb5-4f54-9b9f-91840659b34a
-# ╠═0c83a619-0405-4039-90fd-a09676eb9326
-# ╠═f8cbd064-2eff-4ce3-8a2b-a942d8ccb47d
+# ╟─9bd08151-ce7f-4d57-9dd7-7af79ff42d3f
 # ╠═ce5e566b-b1e3-4e79-9156-4237a170546e
-# ╟─dde0af91-d0ff-4e4d-a4d4-f8fb770987dd
-# ╟─d4549398-3ca3-44ba-b016-ca55bf4056cf
-# ╠═52349a66-88a5-4d88-8523-572e3cb6572b
-# ╠═87de4ba4-7f8e-4d8f-9bda-900df72339f9
-# ╠═d12e5d80-7099-4564-8144-778c812b106c
-# ╠═ae494460-6f76-4562-a6b4-42f0bc6df262
-# ╠═d0d10130-ef6e-4a7f-b3f9-4715f01b737c
+# ╟─4cdad6fd-9976-4bbf-b6de-95cb6a973fc7
 # ╠═aca1dd0e-89b9-4a66-ae0b-2af07cea3636
 # ╠═9c608fab-8fc9-4bf9-be68-96468257e6a8
-# ╠═ba0db11e-4098-4ead-854f-022c123e4d62
+# ╠═7b5d607e-da02-4060-8a9c-60c541fe34aa
+# ╟─dde0af91-d0ff-4e4d-a4d4-f8fb770987dd
+# ╟─d4549398-3ca3-44ba-b016-ca55bf4056cf
+# ╠═d12e5d80-7099-4564-8144-778c812b106c
+# ╠═ae494460-6f76-4562-a6b4-42f0bc6df262
+# ╟─2e3b2b76-c100-4f6d-b5a6-8263da6ca724
+# ╠═730899e1-edd5-4ddc-be98-b193a22bb6f8
+# ╠═87de4ba4-7f8e-4d8f-9bda-900df72339f9
+# ╠═0c83a619-0405-4039-90fd-a09676eb9326
+# ╠═b34323cf-913f-4a1b-b385-f3ae52f320ac
