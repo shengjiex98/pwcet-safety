d1d4980de2df6c85842499bced976726b221011c
Shengjie Xu <shengjiex98@gmail.com>
2024-04-26 13:46:42 -0400

    Remove unnecessary dependencies

--- notebooks/mpc-sim.jl
+++ notebooks/mpc-sim.jl
@@ -20,11 +20,13 @@ begin
 	using Experiments
 
 	push!(LOAD_PATH, "../../")
-	using StatsBase, MATLABControlTest, ControlSystemsBase, CSV, DataFrames
 	
 	using PlutoUI
 	TableOfContents()
 end
+
+# ╔═╡ 39bbf368-4d9b-447a-be29-a0b74f391cf3
+using Revise, MATLABControlTest, ControlSystemsBase, CSV, DataFrames
 
 # ╔═╡ 89e10260-cbfe-49b8-932d-f5a1798a123a
 pwd()
@@ -36,11 +38,8 @@ else
 	ENV["LD_LIBRARY_PATH"] = "$(pwd())/../../MATLABControlTest.jl/src"
 end
 
-# ╔═╡ e234e781-9e83-4b89-8700-f63951b65ab6
-df = CSV.read("../../MATLABControlTest.jl/output-jumping1000-1e3-O1.csv", DataFrame)
-
-# ╔═╡ c2b10163-1380-4466-aa50-07d0ffe10f7f
-ref_signal = df[!,:r]
+# ╔═╡ 566cc4d1-e984-44e4-ab06-4d05d903335a
+ref_signal = CSV.read("../../MATLABControlTest.jl/output-jumping1000-1e3-O1.csv", DataFrame)[!,:r]
 
 # ╔═╡ 22f33099-3217-4d44-bf77-65873eae293f
 function get_ref(t::Real)
@@ -50,28 +49,13 @@ function get_ref(t::Real)
 end
 
 # ╔═╡ 8a2ca62b-f4cd-443e-af7b-ff9ac60caeca
-scatter(df[!,:t], xlim=[0, 100])
 
-# ╔═╡ d43f9865-1358-4544-9bc3-97006a340ef9
-plot(sort(df[!,:t]), (1:nrow(df))/nrow(df))
-
-# ╔═╡ 8a324244-2037-4dae-bd7d-8d5d6dd3359a
-let
-	scatter(df[!,:r], xlim=[0, 100])
-	plot!(df[!,:y])
-end
-
-# ╔═╡ ed5d3ef6-1b05-4a17-b386-0b493e9d848e
-sample(df[!,:t])
 
 # ╔═╡ Cell order:
 # ╠═e37c5e34-0191-11ef-0c30-379698507b64
 # ╠═89e10260-cbfe-49b8-932d-f5a1798a123a
 # ╠═ebd19987-d113-4b8d-a0c7-06c5142310a4
-# ╠═e234e781-9e83-4b89-8700-f63951b65ab6
-# ╠═c2b10163-1380-4466-aa50-07d0ffe10f7f
+# ╠═39bbf368-4d9b-447a-be29-a0b74f391cf3
+# ╠═566cc4d1-e984-44e4-ab06-4d05d903335a
 # ╠═22f33099-3217-4d44-bf77-65873eae293f
 # ╠═8a2ca62b-f4cd-443e-af7b-ff9ac60caeca
-# ╠═d43f9865-1358-4544-9bc3-97006a340ef9
-# ╠═8a324244-2037-4dae-bd7d-8d5d6dd3359a
-# ╠═ed5d3ef6-1b05-4a17-b386-0b493e9d848e
