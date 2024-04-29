e55fa8f7d291b7f903bc5e35c0bd041d3e66f50d
Shengjie Xu <shengjiex98@gmail.com>
2024-04-29 18:43:26 -0400

    Improved data proxy generation (read params from filename)

--- scripts/summarize_data.jl
+++ scripts/summarize_data.jl
@@ -5,38 +5,42 @@ using DelimitedFiles
 push!(LOAD_PATH, "../src")
 using Experiments
 
-SYS = "CC2"
-DIST = Pareto(1.5, 0.03)
-PATH = "../data/nmc-dist/$SYS/$DIST"
-OUTPUT_PATH = "../data-proxy"
-OUTPUT_FILE = "nmc-dist-$SYS-$DIST"
+const SYS = "EWB"
+const DIST = Pareto(1.5, 0.001)
+const PATH = "../data/nmc-dist/$SYS/$DIST"
+const OUTPUT_FILE = "nmc-dist-$SYS-$DIST"
 
-BATCHSIZE = 1_000_000
-H = 100 * 0.02
-Q_VALUES = 0.01:0.01:0.99
+# const JOB_ID = 37312909
+# const PATH = "../data/mpc/$JOB_ID"
+# const OUTPUT_FILE = "mpc-$JOB_ID"
 
-p = 0.99
-i99 = round(Int64, BATCHSIZE * p)
-i_low, i_high = find_intervals(BATCHSIZE, p, 0.05, centered=true)[1]
+const OUTPUT_PATH = "../data-proxy"
 
-function get_quantile(q, h)
-    filename = generate_filename(BATCHSIZE, q, h, th=16)
-    if !isfile("$PATH/$filename.jls")
-        @info "Parameters without valid data" BATCHSIZE q h
-        return [Inf, Inf, Inf]
-    end
-    data = deserialize("$PATH/$filename.jls")
-    [data[i][2] for i in (i_low, i99, i_high)]
+const P = 0.99
+
+function parsefile(filename::String)
+    b_s, q_s, h_s = split(filename, "-")[1:3]
+    b = Int64(parse(Float64, b_s[2:end]))
+    q = parse(Float64, q_s[2:end])
+    h = parse(Float64, h_s[2:end])
+    return b, q, h
 end
 
-quantiles = map(Q_VALUES) do q
-    h = quantile(DIST, q)
-    get_quantile(q, h)
-end |> stack
+@info "Reading files in $PATH..."
+const FILES = readdir(PATH)
+const BATCHSIZE =parsefile(FILES[1])[1]
+@info "$(length(FILES)) files found. Batch size is $BATCHSIZE"
 
-@info size(quantiles)
+const I99 = round(Int64, BATCHSIZE * P)
+const I_LOW, I_HIGH = find_intervals(BATCHSIZE, P, 0.05, centered=true)[1]
 
-serialize("$OUTPUT_PATH/$OUTPUT_FILE.jls", quantiles)
+proxy = map(FILES) do filename
+    b, q, h = parsefile(filename)
+    data = deserialize("$PATH/$filename")
+    vcat([q, h], [data[i][2] for i in [I_LOW, I99, I_HIGH]])
+end |> stack |> transpose
 
-full = vcat(reshape(Q_VALUES, 1, :), reshape(quantile.(DIST, Q_VALUES), 1, :), quantiles)
-writedlm("$OUTPUT_PATH/$OUTPUT_FILE.csv", full', ',')
+@info size(proxy)
+
+serialize("$OUTPUT_PATH/$OUTPUT_FILE.jls", proxy)
+writedlm("$OUTPUT_PATH/$OUTPUT_FILE.csv", proxy, ',')
