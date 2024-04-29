d679d76325f268b58284a88da47286a744096a57
Shengjie Xu <shengjiex98@gmail.com>
2024-04-29 13:55:21 -0400

    Don't modify ENV variables (updated module to no longer require it)

--- notebooks/mpc-sim.jl
+++ notebooks/mpc-sim.jl
@@ -31,13 +31,6 @@ using Revise, MATLABControlTest, ControlSystemsBase, CSV, DataFrames
 # ╔═╡ 89e10260-cbfe-49b8-932d-f5a1798a123a
 pwd()
 
-# ╔═╡ ebd19987-d113-4b8d-a0c7-06c5142310a4
-if "LD_LIBRARY_PATH" in keys(ENV)
-	ENV["LD_LIBRARY_PATH"] = ENV["LD_LIBRARY_PATH"] * ":$(pwd())/../../MATLABControlTest.jl/src"
-else
-	ENV["LD_LIBRARY_PATH"] = "$(pwd())/../../MATLABControlTest.jl/src"
-end
-
 # ╔═╡ 566cc4d1-e984-44e4-ab06-4d05d903335a
 ref_signal = CSV.read("../../MATLABControlTest.jl/output-jumping1000-1e3-O1.csv", DataFrame)[!,:r]
 
@@ -54,7 +47,6 @@ end
 # ╔═╡ Cell order:
 # ╠═e37c5e34-0191-11ef-0c30-379698507b64
 # ╠═89e10260-cbfe-49b8-932d-f5a1798a123a
-# ╠═ebd19987-d113-4b8d-a0c7-06c5142310a4
 # ╠═39bbf368-4d9b-447a-be29-a0b74f391cf3
 # ╠═566cc4d1-e984-44e4-ab06-4d05d903335a
 # ╠═22f33099-3217-4d44-bf77-65873eae293f
