c4d897d524f59bbd7ee44dffcf599801ae21152a
Shengjie Xu <shengjiex98@gmail.com>
2024-07-24 11:38:44 -0400

    Test and Slurm scripts for grid search MPC

--- scripts/test_mpc.sh
+++ scripts/test_mpc.sh
@@ -1,1 +1,2 @@
-SLURM_ARRAY_JOB_ID=1 SLURM_ARRAY_TASK_ID=30 julia --project generate_data_mpc.jl
+# (set -x; SLURM_ARRAY_JOB_ID=1 SLURM_ARRAY_TASK_ID=30 COMPARE_MODE=y julia --project --threads=1 generate_data_mpc.jl)
+(set -x; SLURM_ARRAY_JOB_ID=1 SLURM_ARRAY_TASK_ID=30 COMPARE_MODE=y julia --project --threads=1 generate_grid_mpc.jl)
