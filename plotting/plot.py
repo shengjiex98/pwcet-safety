1d2ea7d177dff611ed49874d4da031c84468fabf
Tingan Zhu <89436540+Tinganz@users.noreply.github.com>
2024-09-15 17:14:38 -0400

    New plotting function

--- plotting/plot.py
+++ plotting/plot.py
@@ -1,32 +1,40 @@
 import plotly.express as px
 import pandas as pd
-import os
 
 def plot(df, 
          x_axis, 
-         hover_fields = ['dev', 'q', 'period', 'utilization'], 
+         hover_fields = ['p99', 'hit_chance', 'period', 'utilization'], 
          color_by = None, 
          filter_fn = None, 
          allpoints = True):
-    if filter_fn is not None:
-        df = df[filter_fn(df)]
     
-
+    if filter_fn is not None:
+        df = filter_fn(df)
+    
     if allpoints:
-        fig = px.scatter(df, x=x_axis, y='dev', hover_data=hover_fields, color=color_by)
+        fig = px.scatter(df, x=x_axis, y='p99', hover_data=hover_fields, color=color_by)
         return fig
     else:
-        df_sorted = df.sort_values(by=df[x_axis])
-        pareto_values = pareto_front(df_sorted, df_sorted['dev'])
-        df_pareto = df_sorted[df_sorted['dev'].isin(pareto_values)]
-        fig = px.scatter(df_pareto, x=x_axis, y='dev', hover_data=hover_fields, color=color_by)
+        df_pareto = pareto_front(df, x_axis)
+        fig = px.scatter(df_pareto, x=x_axis, y='p99', hover_data=hover_fields, color=color_by)
     return fig
+import pandas as pd
 
 def pareto_front(df, col):
+    df_sorted = df.sort_values(by=col, ascending=True)
     pareto_front = []
-    min = float('-inf')  
-    for value in df[col]:
-        if value <= min:
-            pareto_front.append(value)
-            min = value
-    return pareto_front
+    min_dev = float('inf')
+    for index, row in df_sorted.iterrows():
+        if row['p99'] < min_dev:
+            pareto_front.append(row)
+            min_dev = row['p99']  
+    return pd.DataFrame(pareto_front)
+
+def readfile(DATA_PATH, JOB_ID, FILE_NUM):
+    FILES = [f"{DATA_PATH}/{JOB_ID}/{i}.csv" for i in FILE_NUM]
+    dataframes = []
+    for i, file in zip(FILE_NUM, FILES):
+        ref = pd.read_csv(file, names=None, index_col=None)
+        dataframes.append(ref)
+    df = pd.concat(dataframes)
+    return df
